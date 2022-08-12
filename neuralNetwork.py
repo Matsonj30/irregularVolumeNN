541c6b95f74a6d47c2544f02f9a18dcf69649429
Matsonj30 <matsonj2013@gmail.com>
2022-08-12 17:50:53 -0600

    basic reading of inputs from excel

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -1,3 +1,4 @@
+from email.policy import default
 import numpy
 from numpy import ravel
 import random
@@ -34,8 +35,9 @@ class Network:
         }
     
 
-    def activationFunction(number): #sigmoid for now, will probably swap to reLU if we need to normalize our data
-        return 1/(1+ numpy.exp(-abs(number)))
+    def activationFunction(self,number): #sigmoid for now, will probably swap to reLU cause apparently it is king
+        return(max(0, number)) #RELU
+        #return 1/(1+ numpy.exp(-abs(number))) #sigmoid
 
     def sigmoidPrime(number):
         return
@@ -43,53 +45,75 @@ class Network:
         self.inputLayer["values"].append(1) #bias node
         self.hiddenLayer["values"].append(1) #bias node
         
-        data = pandas.ExcelFile("D:/Programming/Repositories/screenerSettings/highVolume.xlsx")
-
-        for inputNode in range(10):
-             self.inputLayer["values"].append(inputNode)
-        for hiddenNode in range(6):
-             self.hiddenLayer["values"].append(hiddenNode)
-        for outPutNode in range(2):
-             self.outputLayer["values"].append(outPutNode)
+       
+        for inputNode in range(self.numberOfInput):
+             self.inputLayer["values"].append(0)
+        for hiddenNode in range(self.numberOfHidden):
+             self.hiddenLayer["values"].append(0)
+        for outputNode in range(self.numberOfOutput):
+             self.outputLayer["values"].append(0)
         self.initializeWeights()
-        
+        self.trainNetwork()
 
     def initializeWeights(self):
         for i in range(len(self.hiddenLayer["values"])-1):
             self.inputLayer["weights"].append([])
             for j in range(len(self.inputLayer["values"])):
                 self.inputLayer["weights"][-1].append(round(random.uniform(-1,1),2))
-        print(self.inputLayer)
+        
 
         for i in range(len(self.outputLayer["values"])):
             self.hiddenLayer["weights"].append([])
             for j in range(len(self.hiddenLayer["values"])):
                 self.hiddenLayer["weights"][-1].append(round(random.uniform(-1,1),2))
-        print(self.hiddenLayer)
-        self.passForward()
+      
+       
 
     def passForward(self):
         answer = 0
         self.hiddenLayer["values"] = numpy.matmul(self.inputLayer["weights"], self.inputLayer["values"]) #pass to hidden 
         for node in range(len(self.hiddenLayer["values"])):
-            self.inputLayer["values"][node] = self.activationFunction(self.inputLayer["values"][node])
-
-            
+            self.hiddenLayer["values"][node] = self.activationFunction(self.hiddenLayer["values"][node])
         self.hiddenLayer["values"] = numpy.insert(self.hiddenLayer["values"], 0, 1) #insert bias (might be a bad idea to do it this way)
-        self.outputLayer["values"] = numpy.matmul(self.hiddenLayer["weights"],self.hiddenLayer["values"])
-  
-        # print(self.hiddenLayer["values"])
-        # print(self.hiddenLayer["weights"])
 
-        # print(self.outputLayer["values"])
-        # print(self.outputLayer["weights"])
+        self.outputLayer["values"] = numpy.matmul(self.hiddenLayer["weights"],self.hiddenLayer["values"])
+        for node in range(len(self.outputLayer["values"])):
+            self.outputLayer["values"][node] = self.activationFunction(self.outputLayer["values"][node])
+
+        print(self.hiddenLayer["values"])
+        print(self.outputLayer["values"])
+       
 
     def trainNetwork(self):
-        return
+        data = pandas.read_excel("D:/Programming/Repositories/irregularVolumeNN/normalizedData.xlsx") #We are going to use 500 data points
+        correctClassifications = 0
+        numberOfClassifications = 0
+        for ticker in range(207): #one epoch, 500 data points
+            for i in range(5): #reset one hot values
+                self.inputLayer["values"][i+1] = 0
+            match data.iloc[ticker, 4:5].values[0]: #mkt cap column in excel file
+                case "Nano":
+                    self.inputLayer["values"][1] = 1
+                case "Micro":
+                      self.inputLayer["values"][2] = 1
+                case "Small":
+                    self.inputLayer["values"][3] = 1
+                case "Mid":
+                    self.inputLayer["values"][4] = 1
+                case "Large":
+                    self.inputLayer["values"][5] = 1
+                case "Mega":
+                    self.inputLayer["values"][6] = 1
+                case _:
+                    print("No mktcap found")
+            self.inputLayer["values"][7] = data.iloc[ticker, 3:4].values[0] #volume node
+            self.inputLayer["values"][8] = data.iloc[ticker, 5:6].values[0] #price
+            self.inputLayer["values"][9] = data.iloc[ticker, 6:7].values[0] #change when found
+            ticker += 1
 
+            self.passForward() #passforwardeachrow
 
 # data = pandas.read_excel("D:/Programming/Repositories/screenerSettings/highVolume.xlsx")
-
 # print(data.iloc[:,4:5])
 
 weight1 = [[1,1,1,1,1,1,1,1,1,1], #6x10
@@ -113,6 +137,5 @@ values = [[1], #10x1 INPUT DICT has to look like this for matmult to work
 
 ] #1 x 1
 
-
-network = Network(0.05, 10, 6, 2)
+network = Network(0.05, 9, 6, 2)
 network.initializeNetwork()
